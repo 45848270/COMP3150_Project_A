c05d677906716925fe26fee64f2e6b7e1bcb9b55
45848270 <shubhamchandra.yadav@students.mq.edu.au>
2022-03-14 13:44:17 +1100

    Movement script for players
    1. Input Axis
    2. Named in Input manager
    3. tested

---
+++ Project A game/Assets/Scripts/InputAxis.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputAxis : MonoBehaviour
+{
+    public const string Player1_X_dir = "Horizontal";
+    public const string Player1_y_dir = "Vertical";
+
+    public const string Player2_X_dir = "Horizontal1";
+    public const string Player2_y_dir = "Vertical1";
+}
